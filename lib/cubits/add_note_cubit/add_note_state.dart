ff749a8c458cace64e810be9d496d2ac71f22433
mostafa abdo <mostafamedhatabdo23@gmail.com>
2024-08-04 18:03:28 +0300

    add add note state

--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
 @immutable
-abstract class AddNoteState{}
-class AddNoteInitial extends AddNoteState{}
+abstract class AddNoteState {}
+
+class AddNoteInitial extends AddNoteState {}
+
+class AddNoteLoading extends AddNoteState {}
+
+class AddNoteSuccess extends AddNoteState {}
+
+class AddNoteFailure extends AddNoteState {
+  final String errMessage;
+
+  AddNoteFailure(this.errMessage);
+}
