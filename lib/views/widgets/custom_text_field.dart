e09d72fac488a5d7aee0194b7945a85a99dcc801
mostafa abdo <mostafamedhatabdo23@gmail.com>
2024-08-04 16:46:55 +0300

    take input and validate data

--- lib/views/widgets/custom_text_field.dart
+++ lib/views/widgets/custom_text_field.dart
@@ -2,29 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constant.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key, required this.hint, this.maxLines=1});
+  const CustomTextField(
+      {super.key, required this.hint, this.maxLines = 1, this.onSaved});
   final String hint;
   final int maxLines;
+  final Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      onSaved: onSaved,
+      validator: (value) {
+        if (value?.isEmpty ?? true) {
+          return 'field is required';
+        } else {
+          return null;
+        }
+      },
       cursorColor: kPrimaryColor,
       maxLines: maxLines,
       decoration: InputDecoration(
-        hintText: hint,
-        border: bordertextfield(),
-        enabledBorder: bordertextfield(),
-        focusedBorder: bordertextfield(kPrimaryColor)
-      ),
+          hintText: hint,
+          border: bordertextfield(),
+          enabledBorder: bordertextfield(),
+          focusedBorder: bordertextfield(kPrimaryColor)),
     );
   }
 
   OutlineInputBorder bordertextfield([color]) {
     return OutlineInputBorder(
         borderRadius: BorderRadius.circular(8),
-        borderSide: BorderSide(
-          color: color?? Colors.white
-        )
-      );
+        borderSide: BorderSide(color: color ?? Colors.white));
   }
 }
