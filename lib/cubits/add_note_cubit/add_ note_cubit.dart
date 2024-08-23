216ec55ee94fcb34a56ffed038b185ba42416ec6
mostafa abdo <mostafamedhatabdo23@gmail.com>
2024-08-23 14:31:09 +0300

    add note bottom sheet done

--- lib/cubits/add_note_cubit/add_ note_cubit.dart
+++ lib/cubits/add_note_cubit/add_ note_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:notes_app/constant.dart';
@@ -7,7 +8,9 @@ import '../../models/note_model.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+  Color colorr=const Color(0xffAC392F);
   addNote(NoteModel noteModel) async {
+    noteModel.color=colorr.value;
     emit(AddNoteLoading());
     try {
       var noteBox = Hive.box<NoteModel>(kNotesBox);
--- lib/views/widgets/color_list_view.dart
+++ lib/views/widgets/color_list_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_%20note_cubit.dart';
 
 class ColorItem extends StatelessWidget {
   const ColorItem({super.key, required this.isActive, required this.color});
@@ -52,6 +54,7 @@ class _ColorListViewState extends State<ColorListView> {
               child: GestureDetector(
                 onTap: () {
                   currentIndex = index;
+                  BlocProvider.of<AddNoteCubit>(context).colorr = colors[index];
                   setState(() {});
                 },
                 child: ColorItem(
